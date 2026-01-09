20fea4cefa73615625751f4361a1a4660b302019
1221107067MMU <imranharis.mohdazli@gmail.com>
2026-01-09 16:30:33 +0800

    backing up

--- trashsortgame resources/trash_example.gd
+++ trashsortgame resources/trash_example.gd
@@ -1,4 +1,19 @@
 extends RigidBody2D
 
+var strength : float = 10
 
-asasasaas
+var held = false
+
+func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
+	if held:
+		linear_velocity = global_position.direction_to(get_global_mouse_position()) * (global_position.distance_to(get_global_mouse_position()) * strength) 
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			held = true
+			print(held)
+		else:
+			held = false
+			print(held)
